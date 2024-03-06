ac567ba550ed126d8df50e1eceb67cfb72b9199f
Fernando Dumont <fernando@fernando.com.br>
2024-03-06 17:34:18 -0300

    Update localTest.sh

--- localTest.sh
+++ localTest.sh
@@ -2,24 +2,31 @@
 
 clear
 
-MACHINE_AGENT_FOLDER=/Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062
+kill_process() {
+	PID=$(ps aux | grep machineagent | grep -v grep | awk '{print $2}')
+	if [ "$PID" != "" ]; then
+        	echo Killing Machine Agent
+	        kill -9 $PID
+	fi
+}
+kill_process
 
-ps -ef | grep machineagent | grep -v grep | awk '{print $2}' | xargs kill -9
+MACHINE_AGENT_FOLDER=/Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062
 
 mvn clean install
 
 rm -rf $MACHINE_AGENT_FOLDER/logs/*.*
 rm -rf $MACHINE_AGENT_FOLDER/monitors/VMWareTagExtension
-rm -rf $MACHINE_AGENT_FOLDER/monitors/VMWareTagExtension-1.0.zip
+rm -rf $MACHINE_AGENT_FOLDER/monitors/VMWareTagExtension*.zip
 
 unzip ./target/VMWareTagExtension-1.0.zip -d $MACHINE_AGENT_FOLDER/monitors/
 
-( cd $MACHINE_AGENT_FOLDER && ./bin/machine-agent -j $JAVA_HOME -d -p pidfile )
+( cd $MACHINE_AGENT_FOLDER; ./bin/machine-agent -j $JAVA_HOME -d -p pidfile )
 
 echo "Press Ctrl+C to exit and kill machine agent"
-trap 'ps -ef | grep machineagent | grep -v grep | awk "{print $2}" | xargs kill -9; exit' INT
+trap 'kill_process; exit' INT
 
-sleep 2
+sleep 5
 
 ( open $MACHINE_AGENT_FOLDER/logs/machine-agent.log )
 
