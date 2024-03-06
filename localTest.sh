bb27247c64ba45454dc9b22a97be1b3235fb1883
Fernando Dumont <fernando@fernando.com.br>
2024-03-06 11:55:32 -0300

    Update localTest.sh

--- localTest.sh
+++ localTest.sh
@@ -2,12 +2,27 @@
 
 clear
 
+MACHINE_AGENT_FOLDER=/Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062
+
+ps -ef | grep machineagent | grep -v grep | awk '{print $2}' | xargs kill -9
+
 mvn clean install
 
-rm -rf /Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062/logs
-rm -rf /Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062/monitors/VMWareTagExtension
-rm -rf /Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062/monitors/VMWareTagExtension-1.0.zip
+rm -rf $MACHINE_AGENT_FOLDER/logs/*.*
+rm -rf $MACHINE_AGENT_FOLDER/monitors/VMWareTagExtension
+rm -rf $MACHINE_AGENT_FOLDER/monitors/VMWareTagExtension-1.0.zip
 
-unzip ./target/VMWareTagExtension-1.0.zip -d /Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062/monitors/
+unzip ./target/VMWareTagExtension-1.0.zip -d $MACHINE_AGENT_FOLDER/monitors/
 
-( cd /Users/fdumont/Downloads/AppD/machineagent-bundle-64bit-linux-aarch64-24.2.1.4062 && java -Dappdynamics.agent.maxMetrics=50000 -jar machineagent.jar)
+( cd $MACHINE_AGENT_FOLDER && ./bin/machine-agent -j $JAVA_HOME -d -p pidfile )
+
+echo "Press Ctrl+C to exit and kill machine agent"
+trap 'ps -ef | grep machineagent | grep -v grep | awk "{print $2}" | xargs kill -9; exit' INT
+
+sleep 2
+
+( open $MACHINE_AGENT_FOLDER/logs/machine-agent.log )
+
+while true; do
+    sleep 1
+done
