8f764ebc7c765674797dbf2d4a28fe4b009e63ca
Fernando Dumont <fernando@fernando.com.br>
2024-03-11 16:14:24 -0300

    Fix publish metrics

--- src/main/java/com/appdynamics/extensions/vmwaretag/VMWareTagExtension.java
+++ src/main/java/com/appdynamics/extensions/vmwaretag/VMWareTagExtension.java
@@ -42,7 +42,7 @@ public class VMWareTagExtension extends AManagedMonitor {
 	private List<Thread> threads = new ArrayList<>();
 	private Boolean isRunning;
 	private int totalMetricsPublished;
-	private String metricPrefix = "Custom Metrics|VMWareTag|";
+	private String metricPrefix = "Custom Metrics|VMWare-TAG|";
 
 	@Override
 	public TaskOutput execute(Map<String, String> configMap, TaskExecutionContext taskExecutionContext)
@@ -54,231 +54,229 @@ public class VMWareTagExtension extends AManagedMonitor {
 			throw new TaskExecutionException("Confluent Config File Not Set, nothing to do");
 		}
 
-		while (true) {
-			this.isRunning = true;
-			yamlConfig = new HashMap<>();
-			Instant startTime = Instant.now();
+		this.isRunning = true;
+		yamlConfig = new HashMap<>();
+		Instant startTime = Instant.now();
 
-			try {
-				logger.info("{} Starting task", Common.getLogHeader(this, "run"));
+		try {
+			this.logger.info("{} Starting task", Common.getLogHeader(this, "run"));
 
-				// READIN CONFIG.YML
-				Yaml yaml = new Yaml();
-				InputStream inputStream = new FileInputStream(
-						taskExecutionContext.getTaskDir() + "/" + configMap.get("config-file"));
-				yamlConfig = yaml.load(inputStream);
+			// READIN CONFIG.YML
+			Yaml yaml = new Yaml();
+			InputStream inputStream = new FileInputStream(
+					taskExecutionContext.getTaskDir() + "/" + configMap.get("config-file"));
+			yamlConfig = yaml.load(inputStream);
 
-				if (yamlConfig.get(Constants.METRIC_PREFIX) != null
-						|| !yamlConfig.get(Constants.METRIC_PREFIX).equals("")) {
-					this.metricPrefix = (String) yamlConfig.get(Constants.METRIC_PREFIX);
+			if (yamlConfig.get(Constants.METRIC_PREFIX) != null
+					&& !yamlConfig.get(Constants.METRIC_PREFIX).equals("")) {
+				this.metricPrefix = (String) yamlConfig.get(Constants.METRIC_PREFIX);
+			}
+			this.logger.info("{} Metric prefix [{}]", Common.getLogHeader(this, "run"),
+					this.metricPrefix);
+
+			// LIST OF CONTROLLERS
+			// VSPHERE DATA IS LOADED ONLY ONCE, BUT THE CUSTOMER MAY HAVE MORE THAN ONE
+			// CONTROLLER, SO IT SHOULD NOT GO TO VSPHERE FOR EACH CONTROLLER, IT SHOULD GO
+			// TO VSPHERE ONLY ONCE AND THEN IN THE LOOP OF SERVERS OF ALL CONTROLLERS TO
+			// PERFORM VALIDATION
+
+			// ==> CONTROLLER CONFIGURATIONS
+			ControllerInfo[] listControllerInfo = new ObjectMapper().convertValue(
+					yamlConfig.get(Constants.CONTROLLERS),
+					ControllerInfo[].class);
+
+			listControllerService = new HashMap<>();
+			for (ControllerInfo ci : listControllerInfo) {
+				listControllerService.put(ci.getControllerHost(), new ControllerService(ci));
+			}
+
+			// ==>VMWARE CONFIGURATIONS
+			VMWareConfig[] listVMWareConfig = new ObjectMapper().convertValue(
+					yamlConfig.get(Constants.VCENTER_SERVERS),
+					VMWareConfig[].class);
+
+			this.listVMWareService = new HashMap<>();
+			for (VMWareConfig vc : listVMWareConfig) {
+				listVMWareService.put(vc.getHost(), new VMWareService(vc));
+			}
+
+			// CREATING THREADS FOR THE CONTROLLERS
+			// LOADING ALL SERVERS (MACHINE AGENTS)
+			listControllerService.forEach((controllerHost, controllerService) -> {
+				ServerThread serverThread = new ServerThread(controllerService);
+				serverThread.setName(controllerHost);
+				threads.add(serverThread);
+			});
+
+			// CREATING THREADS FOR VMWARE SERVERS
+			// LOADING DATACENTERS, CLUSTERS, HOSTS, AND VIRTUAL MACHINES
+			listVMWareService.forEach((host, vmwareService) -> {
+				// RETRIEVING VIRTUAL MACHINES
+				DataCenterThread datacenterThread = new DataCenterThread(vmwareService);
+				datacenterThread.setName(host);
+				threads.add(datacenterThread);
+
+				// RETRIEVING MIGRATION EVENTS
+				EventsThread eventsThread = new EventsThread(vmwareService);
+				eventsThread.setName(host);
+				threads.add(eventsThread);
+			});
+
+			this.logger.debug("{} Starting data capture threads...", Common.getLogHeader(this, "run"));
+			for (Thread thread : threads) {
+				thread.start();
+			}
+
+			this.logger.info("{} ==> Waiting for data capture threads to finish...",
+					Common.getLogHeader(this, "run"));
+			for (Thread thread : threads) {
+				thread.join();
+			}
+
+			this.logger.info("{} Data capture threads finished!", Common.getLogHeader(this, "run"));
+
+			int totalDataCenter = 0;
+			int totalCluster = 0;
+			int totalHost = 0;
+			int totalVMs = 0;
+			int totalEvents = 0;
+			int totalServers = 0;
+			for (Thread thread : threads) {
+				if (thread instanceof DataCenterThread) {
+					totalDataCenter += ((DataCenterThread) thread).getTotalDataCenter();
+					totalCluster += ((DataCenterThread) thread).getTotalCluster();
+					totalHost += ((DataCenterThread) thread).getTotalHost();
+					totalVMs += ((DataCenterThread) thread).getTotalVMs();
+				} else if (thread instanceof EventsThread) {
+					totalEvents += ((EventsThread) thread).getTotalEvents();
+				} else if (thread instanceof ServerThread) {
+					totalServers += ((ServerThread) thread).getTotalServers();
 				}
+			}
+			this.logger.info(
+					"{} Total objects found: Datacenter [{}], Cluster [{}], Host [{}], VM [{}] Events [{}] Servers [{}]",
+					Common.getLogHeader(this, "run"),
+					totalDataCenter, totalCluster, totalHost, totalVMs, totalEvents, totalServers);
 
-				// LIST OF CONTROLLERS
-				// VSPHERE DATA IS LOADED ONLY ONCE, BUT THE CUSTOMER MAY HAVE MORE THAN ONE
-				// CONTROLLER, SO IT SHOULD NOT GO TO VSPHERE FOR EACH CONTROLLER, IT SHOULD GO
-				// TO VSPHERE ONLY ONCE AND THEN IN THE LOOP OF SERVERS OF ALL CONTROLLERS TO
-				// PERFORM VALIDATION
+			// MATCHING MACHINE AGENTS WITH VIRTUAL MACHINES
+			threads = new ArrayList<>();
+			this.logger.debug("{} Starting match threads...", Common.getLogHeader(this, "run"));
+			listControllerService.forEach((host, controlerService) -> {
+				Thread matchThread = new MatchThread(controlerService, new ArrayList<>(listVMWareService.values()));
+				matchThread.setName(host);
+				matchThread.start();
+				threads.add(matchThread);
+			});
 
-				// ==> CONTROLLER CONFIGURATIONS
-				ControllerInfo[] listControllerInfo = new ObjectMapper().convertValue(
-						yamlConfig.get(Constants.CONTROLLERS),
-						ControllerInfo[].class);
+			this.logger.info("{} ==> Waiting for data match threads to finish...", Common.getLogHeader(this, "run"));
+			for (Thread thread : threads) {
+				thread.join();
+			}
+			this.logger.info("{} Data match threads finished!", Common.getLogHeader(this, "run"));
 
-				listControllerService = new HashMap<>();
-				for (ControllerInfo ci : listControllerInfo) {
-					listControllerService.put(ci.getControllerHost(), new ControllerService(ci));
+			int totalServerTagged = 0;
+			for (Thread thread : threads) {
+				if (thread instanceof MatchThread) {
+					totalServerTagged += ((MatchThread) thread).getTotalServerTagged();
 				}
+			}
+			this.logger.info("{} Total servers to create tags (matched) [{}]",
+					Common.getLogHeader(this, "run"), totalServerTagged);
 
-				// ==>VMWARE CONFIGURATIONS
-				VMWareConfig[] listVMWareConfig = new ObjectMapper().convertValue(
-						yamlConfig.get(Constants.VCENTER_SERVERS),
-						VMWareConfig[].class);
+			// PUBLISHING TAGS
+			threads = new ArrayList<>();
+			this.logger.debug("{} Starting publish tags threads...", Common.getLogHeader(this, "run"));
+			listControllerService.forEach((host, controlerService) -> {
+				Thread publishThread = new PublishTagsThread(controlerService,
+						String.valueOf(yamlConfig.get(Constants.FORMAT_DATE)));
+				publishThread.setName(host);
+				publishThread.start();
+				threads.add(publishThread);
+			});
 
-				this.listVMWareService = new HashMap<>();
-				for (VMWareConfig vc : listVMWareConfig) {
-					listVMWareService.put(vc.getHost(), new VMWareService(vc));
-				}
+			this.logger.info("{} ==> Waiting for publish tag threads to finish...", Common.getLogHeader(this, "run"));
+			for (Thread thread : threads) {
+				thread.join();
+			}
+			this.logger.info("{} Publish tags threads finished!", Common.getLogHeader(this, "run"));
 
-				// CREATING THREADS FOR THE CONTROLLERS
-				// LOADING ALL SERVERS (MACHINE AGENTS)
-				listControllerService.forEach((controllerHost, controllerService) -> {
-					ServerThread serverThread = new ServerThread(controllerService);
-					serverThread.setName(controllerHost);
-					threads.add(serverThread);
-				});
-
-				// CREATING THREADS FOR VMWARE SERVERS
-				// LOADING DATACENTERS, CLUSTERS, HOSTS, AND VIRTUAL MACHINES
-				listVMWareService.forEach((host, vmwareService) -> {
-					// RETRIEVING VIRTUAL MACHINES
-					DataCenterThread datacenterThread = new DataCenterThread(vmwareService);
-					datacenterThread.setName(host);
-					threads.add(datacenterThread);
-
-					// RETRIEVING MIGRATION EVENTS
-					EventsThread eventsThread = new EventsThread(vmwareService);
-					eventsThread.setName(host);
-					threads.add(eventsThread);
-				});
-
-				logger.debug("{} Starting data capture threads...", Common.getLogHeader(this, "run"));
-				for (Thread thread : threads) {
-					thread.start();
-				}
-
-				logger.info("{} ==> Waiting for data capture threads to finish...",
-						Common.getLogHeader(this, "run"));
-				for (Thread thread : threads) {
-					thread.join();
-				}
-
-				logger.info("{} Data capture threads finished!", Common.getLogHeader(this, "run"));
-
-				int totalDataCenter = 0;
-				int totalCluster = 0;
-				int totalHost = 0;
-				int totalVMs = 0;
-				int totalEvents = 0;
-				int totalServers = 0;
-				for (Thread thread : threads) {
-					if (thread instanceof DataCenterThread) {
-						totalDataCenter += ((DataCenterThread) thread).getTotalDataCenter();
-						totalCluster += ((DataCenterThread) thread).getTotalCluster();
-						totalHost += ((DataCenterThread) thread).getTotalHost();
-						totalVMs += ((DataCenterThread) thread).getTotalVMs();
-					} else if (thread instanceof EventsThread) {
-						totalEvents += ((EventsThread) thread).getTotalEvents();
-					} else if (thread instanceof ServerThread) {
-						totalServers += ((ServerThread) thread).getTotalServers();
-					}
-				}
-				logger.info(
-						"{} Total objects found: Datacenter [{}], Cluster [{}], Host [{}], VM [{}] Events [{}] Servers [{}]",
-						Common.getLogHeader(this, "run"),
-						totalDataCenter, totalCluster, totalHost, totalVMs, totalEvents, totalServers);
-
-				// MATCHING MACHINE AGENTS WITH VIRTUAL MACHINES
-				threads = new ArrayList<>();
-				logger.debug("{} Starting match threads...", Common.getLogHeader(this, "run"));
+			threads = new ArrayList<>();
+			// THE MATCH WILL IDENTIFY ONLY THE HOSTS THAT HAVE VMs ON THE CONTROLLER'S
+			// SERVERS, PUBLISH METRICS ONLY FOR THESE FOUND HOSTS
+			this.totalMetricsPublished = 0;
+			if (yamlConfig.get(Constants.PUBLISH_METRICS) != null
+					&& (boolean) yamlConfig.get(Constants.PUBLISH_METRICS)) {
+				this.logger.info("{} ==> Publishing metrics values...", Common.getLogHeader(this, "run"));
 				listControllerService.forEach((host, controlerService) -> {
-					Thread matchThread = new MatchThread(controlerService, new ArrayList<>(listVMWareService.values()));
-					matchThread.setName(host);
-					matchThread.start();
-					threads.add(matchThread);
-				});
+					for (Server server : controlerService.listServerTagged) {
+						try {
+							// metricPrefix: "Custom Metrics|VMWareTag"
+							String metricName = server.getDatacenterName() +
+									"|" + server.getClusterName() +
+									"|" + server.getHostName() + "|";
 
-				logger.info("{} ==> Waiting for data match threads to finish...", Common.getLogHeader(this, "run"));
-				for (Thread thread : threads) {
-					thread.join();
-				}
-				logger.info("{} Data match threads finished!", Common.getLogHeader(this, "run"));
+							// publicMetric("Up", 1,
+							// MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
+							// MetricWriter.METRIC_TIME_ROLLUP_TYPE_SUM,
+							// MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
 
-				int totalServerTagged = 0;
-				for (Thread thread : threads) {
-					if (thread instanceof MatchThread) {
-						totalServerTagged += ((MatchThread) thread).getTotalServerTagged();
-					}
-				}
-				logger.info("{} Total servers to create tags (matched) [{}]",
-						Common.getLogHeader(this, "run"), totalServerTagged);
+							// publicMetric(baseMetricName,
+							// "Overall CPU Usage",
+							// String.valueOf(server.getHostStats().getOverallCpuUsage()));
 
-				// PUBLISHING TAGS
-				threads = new ArrayList<>();
-				logger.debug("{} Starting publish tags threads...", Common.getLogHeader(this, "run"));
-				listControllerService.forEach((host, controlerService) -> {
-					Thread publishThread = new PublishTagsThread(controlerService,
-							String.valueOf(yamlConfig.get(Constants.FORMAT_DATE)));
-					publishThread.setName(host);
-					publishThread.start();
-					threads.add(publishThread);
-				});
+							publicMetric(metricName + "Overall CPU Usage %",
+									server.getHostStats().getOverallCpuUsagePerc(),
+									"OBSERVATION", "AVERAGE", "COLLECTIVE");
 
-				logger.info("{} ==> Waiting for publish tag threads to finish...", Common.getLogHeader(this, "run"));
-				for (Thread thread : threads) {
-					thread.join();
-				}
-				logger.info("{} Publish tags threads finished!", Common.getLogHeader(this, "run"));
+							// publicMetric(baseMetricName,
+							// "Memory Size",
+							// String.valueOf(server.getHostStats().getMemorySize()));
 
-				threads = new ArrayList<>();
-				// THE MATCH WILL IDENTIFY ONLY THE HOSTS THAT HAVE VMs ON THE CONTROLLER'S
-				// SERVERS, PUBLISH METRICS ONLY FOR THESE FOUND HOSTS
-				this.totalMetricsPublished = 0;
-				if (yamlConfig.get(Constants.PUBLISH_METRICS) != null
-						&& (boolean) yamlConfig.get(Constants.PUBLISH_METRICS)) {
-					logger.info("{} ==> Publishing metrics values...", Common.getLogHeader(this, "run"));
-					listControllerService.forEach((host, controlerService) -> {
-						for (Server server : controlerService.listServerTagged) {
-							try {
-								// metricPrefix: "Custom Metrics|VMWareTag"
-								String metricName = server.getDatacenterName() +
-										"|" + server.getClusterName() +
-										"|" + server.getHostName();
+							// publicMetric(baseMetricName,
+							// "Overall Memory Usage",
+							// String.valueOf(server.getHostStats().getOverallMemoryUsage()));
 
-								publicMetric(metricName + "|HeartBeat", 1, "OBSERVATION", "SUM", "COLLECTIVE");
+							// publicMetric(metricName,
+							// metricName +"Overall Memory Usage %",
+							// String.valueOf(server.getHostStats().getOverallMemoryPerc()));
+							publicMetric(metricName + "Overall Memory Usage %",
+									server.getHostStats().getOverallMemoryPerc(),
+									"OBSERVATION", "AVERAGE", "COLLECTIVE");
 
-								// publicMetric(baseMetricName,
-								// "Overall CPU Usage",
-								// String.valueOf(server.getHostStats().getOverallCpuUsage()));
+							// publicMetric(metricName,
+							// "Total Virtual Machines",
+							// String.valueOf(server.getHostStats().getTotalVirtualMachine()));
+							publicMetric(metricName + "Total Virtual Machines",
+									server.getHostStats().getTotalVirtualMachine(),
+									"OBSERVATION", "AVERAGE", "COLLECTIVE");
 
-								publicMetric(metricName + "|Overall CPU Usage %",
-										server.getHostStats().getOverallCpuUsagePerc(),
-										"OBSERVATION", "AVERAGE", "COLLECTIVE");
-
-								// publicMetric(baseMetricName,
-								// "Memory Size",
-								// String.valueOf(server.getHostStats().getMemorySize()));
-
-								// publicMetric(baseMetricName,
-								// "Overall Memory Usage",
-								// String.valueOf(server.getHostStats().getOverallMemoryUsage()));
-
-								// publicMetric(metricName,
-								// "Overall Memory Usage %",
-								// String.valueOf(server.getHostStats().getOverallMemoryPerc()));
-								publicMetric(metricName + "|Overall Memory Usage %",
-										server.getHostStats().getOverallMemoryPerc(),
-										"OBSERVATION", "AVERAGE", "COLLECTIVE");
-
-								// publicMetric(metricName,
-								// "Total Virtual Machines",
-								// String.valueOf(server.getHostStats().getTotalVirtualMachine()));
-								publicMetric(metricName + "|Total Virtual Machines",
-										server.getHostStats().getTotalVirtualMachine(),
-										"OBSERVATION", "AVERAGE", "COLLECTIVE");
-
-							} catch (Exception e) {
-								logger.error("{} {}...",
-										Common.getLogHeader(this, "run"),
-										e.getMessage(), e);
-							}
+						} catch (Exception e) {
+							logger.error("{} {}...",
+									Common.getLogHeader(this, "run"),
+									e.getMessage(), e);
 						}
-					});
+					}
+				});
 
-				}
-
-				logger.info("{} Total metrics published [{}]", Common.getLogHeader(this, "run"),
-						this.totalMetricsPublished);
-
-			} catch (Exception e) {
-				logger.error("{} Exception on running task {}", Common.getLogHeader(this, "run"), e.getMessage(), e);
 			}
 
-			try {
-				Duration duration = Duration.between(startTime, Instant.now());
-				logger.info("{} Execution time {}s, waiting next round {}ms", Common.getLogHeader(this, "run"),
-						duration.getSeconds(), yamlConfig.get(Constants.FREQUENCY));
-				this.isRunning = false;
-				Thread.sleep(
-						Integer.valueOf(yamlConfig.get(Constants.FREQUENCY).toString()));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			logger.info("{} Total metrics published [{}]", Common.getLogHeader(this, "run"),
+					this.totalMetricsPublished);
+
+		} catch (Exception e) {
+			logger.error("{} Exception on running task {}", Common.getLogHeader(this, "run"), e.getMessage(), e);
 		}
 
-		// VMWareTagExtensionTask task = new VMWareTagExtensionTask(
-		// tasksExecutionServiceProvider,
-		// this.getContextConfiguration());
-		// tasksExecutionServiceProvider.submit("VMWareTagExtensionTask", task);
+		try {
+			Duration duration = Duration.between(startTime, Instant.now());
+			logger.info("{} Execution time {}s, waiting next round {}ms", Common.getLogHeader(this, "run"),
+					duration.getSeconds(), yamlConfig.get(Constants.FREQUENCY));
+			this.isRunning = false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
 
 	}
 
@@ -297,10 +295,8 @@ public class VMWareTagExtension extends AManagedMonitor {
 	protected void publicMetric(String metricName, Object metricValue,
 			String aggregation, String timeRollup, String cluster) throws Exception {
 		this.totalMetricsPublished += 1;
-		logger.info("{} Publishing metric {}{}={}", Common.getLogHeader(this, "publicMetric"),
-				this.metricPrefix,
-				metricName,
-				metricValue);
+		this.logger.debug("Printing Metric [{}/{}/{}] [{}]=[{}]", aggregation, timeRollup, cluster,
+				this.metricPrefix + metricName, metricValue);
 
 		MetricWriter metricWriter = getMetricWriter(this.metricPrefix + metricName,
 				aggregation,
